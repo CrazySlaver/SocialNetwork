f05fef9aa6c27b83439132a43e40ab09d63fefa6
CrazySlaver <valera.pogor@gmail.com>
2015-10-04 02:20:24 +0300

    First commit.
    Create MVC project and database.

---
+++ MusicWave/MusicWave/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MusicWave
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
